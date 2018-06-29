505a11fc0848b8ca11b8806bc235d124ef5f247b
InJung Chung <mu29@yeoubi.net>
2018-06-29 21:41:26 +0900

    주석 및 실행 스크립트를 추가한다

--- artworks/index.js
+++ artworks/index.js
@@ -3,6 +3,14 @@
 const datastore = require('@google-cloud/datastore')()
 const RESULTS_PER_PAGE = 15
 
+// [START artworks_recent_get]
+/**
+ * 최신 작품 목록을 보여줍니다. 한 번에 최대 15개의 결과를 보여줄 수 있습니다.
+ *
+ * @param {Object} request Cloud Function의 request context 입니다.
+ * @param {String} request.query.cursor 목록의 첫 아이템의 위치입니다.
+ * @param {Object} response Cloud Function의 response context 입니다.
+ */
 exports.recent = (request, response) => {
   const query = datastore
     .createQuery('Artwork')
@@ -25,3 +33,4 @@ exports.recent = (request, response) => {
     })
   })
 }
+// [END artworks_recent_get]
