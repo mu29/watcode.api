cfda6a49eb5cc8b852234ecc83099226abdff67e
InJung Chung <mu29@yeoubi.net>
2018-07-16 22:00:00 +0900

    작품의 인기도를 올려주는 함수를 분리한다

--- src/modules/updateView.js
+++ src/modules/updateView.js
@@ -1,4 +1,5 @@
 import { update } from '../services/database'
+import updatePopularity from './updatePopularity'
 
 /**
  * 작품의 조회 횟수를 늘려줍니다.
@@ -10,33 +11,10 @@ import { update } from '../services/database'
 export default async (request, response) => {
   const pathRegex = /artworks\/(\d+)\/views/
   const code = parseInt(request.url.match(pathRegex)[1])
-  const today = parseInt(Date.now() / (86400 * 1000))
 
   await Promise.all([
     update(['Artwork', code], artwork => ({ views: (artwork.views || 0) + 1 })),
-    update(['Counter', code], (counter) => {
-      // 일별 조회 수 소스를 가져옵니다.
-      const source = counter.source || Array(30).fill(0)
-      // 날짜가 변경된 경우 가장 오래된 조회 데이터를 삭제합니다.
-      if (today !== counter.updatedAt) {
-        source.shift()
-        source.push(0)
-      }
-      // 오늘 조회 수를 늘려줍니다.
-      source.push(source.pop() + 1)
-      source.reverse()
-
-      const sum = (src, count) => src.slice(0, count).reduce((e, r) => e + r, 0)
-
-      return {
-        code,
-        daily: sum(source, 2),
-        weekly: sum(source, 7),
-        monthly: sum(source, 30),
-        source: source.reverse(),
-        updatedAt: today,
-      }
-    }),
+    updatePopularity(1),
   ])
 
   response.status(201).end()
