b20a35bc67f68a6c36994a7d8d636bd2cc017f5e
InJung Chung <mu29@yeoubi.net>
2018-07-18 17:16:58 +0900

    댓글 삭제 함수를 추가한다

--- src/handlers/comments.js
+++ src/handlers/comments.js
@@ -1,17 +1,27 @@
-import { readCommentList, createComment } from '../modules'
+import { readCommentList, createComment, deleteComment } from '../modules'
 
 export default (request, response) => {
-  if (!request.url.match(/artworks\/(\d+)\/comments.*/)) {
-    response.status(422).send({ message: '함수의 경로가 잘못되었습니다.' })
-    return
-  }
-
   switch (request.method) {
     case 'GET':
+      if (!request.url.match(/artworks\/(\d+)\/comments.*/)) {
+        response.status(422).send({ message: '함수의 경로가 잘못되었습니다.' })
+        return
+      }
       readCommentList(request, response)
       break
     case 'POST':
+      if (!request.url.match(/artworks\/(\d+)\/comments.*/)) {
+        response.status(422).send({ message: '함수의 경로가 잘못되었습니다.' })
+        return
+      }
       createComment(request, response)
+      break
+    case 'DELETE':
+      if (!request.url.match(/comments\/(\d+)/)) {
+        response.status(422).send({ message: '함수의 경로가 잘못되었습니다.' })
+        return
+      }
+      deleteComment(request, response)
       break
     case 'OPTIONS':
       response.status(200).end()
